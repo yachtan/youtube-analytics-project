a675228807a79d0bf4ff04f3f7c2ce90669c3ccc
yachanova_tania <tan9l_85@mail.ru>
2023-09-30 17:03:59 +0300

    homework_6

--- src/video.py
+++ src/video.py
@@ -7,20 +7,32 @@ class Video:
 
     def __init__(self, video_id):
         self.video_id = video_id
-        self.youtube = build('youtube', 'v3', developerKey=Video.api_key)
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                    id=self.video_id).execute()
-        #название видео
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        #ссылка на видео
+        # ссылка на видео
         self.url = 'https://www.youtube.com/watch?v=' + self.video_id
-        #количество просмотров
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        #количество лайков
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+
+        self.video_response = Video.get_service().videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                                id=self.video_id).execute()
+
+        try:
+            # название видео
+            self.title = self.video_response['items'][0]['snippet']['title']
+        except IndexError:
+            self.title = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title = self.video_response['items'][0]['snippet']['title']
+            # количество просмотров
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            # количество лайков
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return f'{self.video_title}'
+        return f'{self.title}'
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=cls.api_key)
 
 
 class PLVideo(Video):
